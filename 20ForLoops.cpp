7fabb735fc64d45172a2ec1e68de1dd813481857
William M <artofmediel@gmail.com>
2023-05-03 10:52:15 +0800

    Add files via upload

---
+++ 20ForLoops.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    for(int index = 10; index >=0; index-=2){
+        std::cout << "Index count : " << index << "\n";
+    }
+
+    std::cout << "Happy New Year!\n";
+
+    return 0;
+}
